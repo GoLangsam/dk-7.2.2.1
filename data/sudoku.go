98feecffbdb3a7c569de03f9058c96642a279750
GoLangsam <GoLangsam.APA@a-bc.net>
2019-06-19 16:32:08 +0630

    add sudoku-data

---
+++ data/sudoku.go
@@ -0,0 +1,106 @@
+// Copyright 2016 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package data
+
+/*
+Sodoku:
+
+Square (r,c)
+Rows: r 1...9
+Cols: c 1...9
+Blok: b 1...9
+
+k = Digit
+
+---
+Then every sudoku square S = (s(ij)) corresponds naturally to the solution of a master exact cover problem
+whose 9 * 9 * 9 = 729 options are
+`	p(ij)	r(ik)	c(jk)	b(x(ij)k)' for 0 < i,j < 10, 0 < k < 10, and x = 3*(i/3) + j/3 (30)
+
+and whose 4 * 9 * 9 = 324 items are
+	p(ij)	r(ik)	c(jk)	b(x(ij)k)
+
+The reason is that option (30) is chosen with parameters (i, j, k ) if and only if
+	s(ij) = k
+
+Item p(ij) must be covered by exactly one of the nine options that fill cell s(ij)
+item r(ik) must be covered by exactly one of the nine options that put k in row i...
+item b(xk) must be covered by exactly one of the nine options that put k in box x.
+
+Got it?
+*/
+
+func sudokuBlock(i, j int) int { return (i/3)*3 + j/3 }
+
+// Sudoku returns a Sudoku problem for the given hint.
+func Sudoku(hint SudokuHint) [][]string {
+
+	const N = 9 // Size of sudoku square
+	const D = 9 // No of different digits in sudoku square
+
+	plcs := IDs("P-", N) // Place
+	rows := IDs("R-", N) // Rows
+	cols := IDs("C-", N) // Cols
+	blks := IDs("B-", N) // Blocks
+
+	p := [][]string{}
+	r := [][]string{}
+	c := [][]string{}
+	b := [][]string{}
+
+	for _, plc := range plcs {
+		p = append(p, IDs(plc+"-", N))
+	}
+
+	for _, row := range rows {
+		r = append(r, IDs(row+"V=", D))
+	}
+
+	for _, col := range cols {
+		c = append(c, IDs(col+"V=", D))
+	}
+
+	for _, blk := range blks {
+		b = append(b, IDs(blk+"V=", D))
+	}
+
+	itemS := []string{}
+	optaS := [][]string{}
+
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			if hint[i][j] == 0 {
+				itemS = append(itemS,
+					p[i][j],
+				)
+			}
+			if !hint.IsDinR(j, i) { // k=j, i=i
+				itemS = append(itemS,
+					r[i][j],
+				)
+			}
+			if !hint.IsDinC(j, i) { // k=j, j=i
+				itemS = append(itemS,
+					c[i][j],
+				)
+			}
+			if !hint.IsDinB(j, i) { // k=j, x=i
+				itemS = append(itemS,
+					b[i][j],
+				)
+			}
+			for k := range hint.Hint(i, j) {
+				x := sudokuBlock(i, j)
+				if hint[i][j] == 0 && !hint.IsDinR(k, i) && !hint.IsDinC(k, j) && !hint.IsDinB(k, x) {
+					optaS = append(optaS, []string{
+						p[i][j], r[i][k], c[j][k], b[x][k],
+					})
+				}
+			}
+		}
+	}
+
+	return append([][]string{itemS, {}}, optaS...) // items & separator & options
+}
---
+++ data/sudoku_hint.go
@@ -0,0 +1,95 @@
+// Copyright 2016 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package data
+
+func Hint29a() SudokuHint {
+	return [][]int{
+		{0, 0, 3, 0, 1, 0, 0, 0, 0},
+		{4, 1, 5, 0, 0, 0, 0, 9, 0},
+		{2, 0, 6, 5, 0, 0, 3, 0, 0},
+		{5, 0, 0, 0, 8, 0, 0, 0, 9},
+		{0, 7, 0, 9, 0, 0, 3, 2, 0},
+		{0, 3, 8, 0, 0, 4, 0, 6, 0},
+		{0, 0, 0, 2, 6, 0, 4, 0, 3},
+		{0, 0, 0, 3, 0, 0, 0, 0, 8},
+		{3, 2, 0, 0, 0, 7, 9, 5, 0},
+	}
+}
+
+func Hint29b() SudokuHint {
+	return [][]int{
+		{0, 0, 0, 0, 0, 0, 3, 0, 0},
+		{1, 0, 0, 4, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 0, 5},
+		{9, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 2, 6, 0, 0},
+		{0, 0, 0, 0, 5, 3, 0, 0, 0},
+		{0, 5, 0, 8, 0, 0, 0, 0, 0},
+		{0, 0, 0, 9, 0, 0, 0, 7, 0},
+		{0, 8, 3, 0, 0, 0, 0, 4, 0},
+	}
+}
+
+func Hint29c() SudokuHint {
+	return [][]int{
+		{0, 3, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 4, 0, 0, 1, 0, 0},
+		{0, 5, 0, 0, 0, 0, 0, 9, 0},
+		{2, 0, 0, 0, 0, 0, 6, 0, 4},
+		{0, 0, 0, 0, 3, 5, 0, 0, 0},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0},
+		{4, 0, 0, 6, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 5, 0},
+		{0, 9, 0, 0, 0, 0, 0, 0, 0},
+	}
+}
+
+// SudokuHint represents a partially filled
+// Sudoku grid awaiting to be solved.
+type SudokuHint [][]int
+
+var all = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+// Hint returns the digits applicable to cell i,j
+// which is either a predefined digit, or all.
+func (a SudokuHint) Hint(i, j int) []int {
+	if a[i][j] == 0 {
+		return all
+	}
+	return []int{a[i][j]}
+}
+
+func (a SudokuHint) IsDinR(k, i int) bool {
+	d := k + 1
+	for _, ki := range a[i] {
+		if ki == d {
+			return true
+		}
+	}
+	return false
+}
+
+func (a SudokuHint) IsDinC(k, j int) bool {
+	d := k + 1
+	for _, r := range a {
+		if r[j] == d {
+			return true
+		}
+	}
+	return false
+}
+
+func (a SudokuHint) IsDinB(k, b int) bool {
+	d := k + 1
+	x := sudokuBlock
+	for hi, r := range a {
+		for hj, ki := range r {
+			if ki == d && x(hi, hj) == b {
+				return true
+			}
+		}
+	}
+	return false
+}

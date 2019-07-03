1feb50bb4791d1b70505dd850f5b7f8380078a96
GoLangsam <GoLangsam.APA@a-bc.net>
2019-07-03 16:58:40 +0630

    use standard qqq.log.go

--- cmd/xcc/qqq.log.go
+++ cmd/xcc/qqq.log.go
@@ -2,52 +2,36 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:pattern "github.com/GoLangsam/do/qqq/qqq.log.go" 
+
 package main
 
 import "log"
 
-// se_ is the logging function
 var se_ = log.Print
-
-// see is the logging function
 var see = log.Println
-
-// sef is the logging function
 var sef = log.Printf
 
-// di_ logs the fatal error
-var di_ = log.Fatal
-
-// die logs the fatal error
-var die = log.Fatalln
-
-// die logs the fatal error
-var dif = log.Fatalf
+var di_ = log.Panic
+var die = log.Panicln
+var dif = log.Panicf
 
 var verbose bool
 
-// qq_ logs iff verbose
 func qq_(args ...interface{}) {
 	if verbose {
 		se_(args...)
 	}
 }
 
-// qqq logs iff verbose
 func qqq(args ...interface{}) {
 	if verbose {
 		see(args...)
 	}
 }
 
-// qqf logs iff verbose
 func qqf(format string, args ...interface{}) {
 	if verbose {
 		sef(format, args...)
 	}
-}
-
-func init() {
-	log.SetFlags(0)
-	log.SetPrefix("")
 }

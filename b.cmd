f2be8b7f080949b3e088354fed5b221822d69a47
GoLangsam <GoLangsam.APA@a-bc.net>
2019-07-17 20:16:15 +0630

    b.cmd: reduce wait time

--- b.cmd
+++ b.cmd
@@ -1,12 +1,12 @@
 go fmt  ./...
 go test ./... 
 
-@Choice /C YN /D Y /T 7 /M ">>>>>>>>>> Continue?"
+@Choice /C YN /D Y /T 3 /M ">>>>>>>>>> Continue?"
 @If ErrorLevel = 2 goto done
 
 go vet -composites=false ./...
 
-@Choice /C YN /D Y /T 7 /M ">>>>>>>>>> Continue?"
+@Choice /C YN /D Y /T 3 /M ">>>>>>>>>> Continue?"
 @If ErrorLevel = 2 goto done
 
 @r.cmd
